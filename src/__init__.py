ef7d25535888362cda348c66caefaa9c0c170514
Berat Atmaca <beratatmaca@live.com>
2024-10-23 00:22:52 +0300

    Remove unnecessary files

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+import sys
+
+__version__ = '0.0.1'
+
+if sys.version_info.major < 3:
+    raise Exception("PyRigidBody requires at least python 3.X to run.")
+
+from .Marker import Marker
+from .RigidBody import RigidBody
